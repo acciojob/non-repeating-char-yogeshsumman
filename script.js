2b94591cb0c4aeec81a04d6e382c3f3846d09256
Yogesh Summan <yogeshsumman2001@gmail.com>
2025-03-02 15:10:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+	if(str.length===0)return;
+
+	//creating a map to store the frequency of each character
+	const charfrequency ={};
+	//first pass count the frequency of each character
+	for(let char of str){
+		charFrequency[char]=(charFrequency[char]||0)+1;
+	}
+
+	//Second Pass:find the first character with a frequency of 1
+
+	for(let char of str){
+		if(charfrequency[char]===1){
+			return char;
+		}
+	}
+	return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
